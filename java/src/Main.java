2d41444e7e2dc7323347228660142504b01b3bdb
Antonio <antoniorodrigo92@gmail.com>
2021-03-06 19:03:27 +0000

    Antes de testar todos os sensores

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main (String[] args) {
-//        ConnectToDBSID h1 = new ConnectToDBSID("sid2021", "g07", "sensorh1", "col1");
+//        ConnectToDBSID h1 = new ConnectToDBSID("sid2021", "g07", "sensorh1", "sensorh1");
 //        h1.start();
-//        ConnectToDBSID h2 = new ConnectToDBSID("sid2021", "g07", "sensorh2", "col1");
+//        ConnectToDBSID h2 = new ConnectToDBSID("sid2021", "g07", "sensorh2", "sensorh2");
 //        h2.start();
-//        ConnectToDBSID l1 = new ConnectToDBSID("sid2021", "g07", "sensorl1", "col1");
+//        ConnectToDBSID l1 = new ConnectToDBSID("sid2021", "g07", "sensorl1", "sensorl1");
 //        l1.start();
-//        ConnectToDBSID l2 = new ConnectToDBSID("sid2021", "g07", "sensorl2", "col1");
+//        ConnectToDBSID l2 = new ConnectToDBSID("sid2021", "g07", "sensorl2", "sensorl2");
 //        l2.start();
-        ConnectToDBSID t1 = new ConnectToDBSID("sid2021", "g07", "sensort1", "col1");
+        ConnectToDBSID t1 = new ConnectToDBSID("sid2021", "g07", "sensort1", "sensort1");
         t1.start();
-//        ConnectToDBSID t2 = new ConnectToDBSID("sid2021", "g07", "sensort2", "col1");
+//        ConnectToDBSID t2 = new ConnectToDBSID("sid2021", "g07", "sensort2", "sensort2");
 //        t2.start();
     }
 }
