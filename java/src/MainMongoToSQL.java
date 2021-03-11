fb62fc66bf177cd27413cafca88ec89ddec7519a
Rui Gomes <rui_filipe_gomes@iscte-iul.pt>
2021-03-11 03:16:16 +0000

    Teste de escrita para MySQL. O registo já é criado.

--- java/src/MainMongoToSQL.java
+++ java/src/MainMongoToSQL.java
@@ -5,6 +5,7 @@ import com.mongodb.client.MongoDatabase;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.LinkedList;
 
 public class MainMongoToSQL {
     private static final String SOURCE_URI = "mongodb+srv://sid2021:sid2021@sid.yingw.mongodb.net/g07?retryWrites=true&w=majority";
@@ -14,13 +15,15 @@ public class MainMongoToSQL {
     public static void main (String[] args) {
         try {
             Connection sqlConn = DriverManager.getConnection(TARGET_URL, "aluno", "aluno");
-            MongoClientURI sourceURI = new MongoClientURI(SOURCE_URI);
-            MongoDatabase sourceDB = new MongoClient(sourceURI).getDatabase("g07");
+            //MongoClientURI sourceURI = new MongoClientURI(SOURCE_URI);
+            //MongoDatabase sourceDB = new MongoClient(sourceURI).getDatabase("g07");
 
             SqlSender sender = new SqlSender(sqlConn);
+            //teste ainda sem valores do mongo
+            sender.send(sqlConn,new LinkedList<>());
 
-            MongoToSql st1 = new MongoToSql(sourceDB, "sensort1", sqlConn, sender);
-            st1.run();
+            //MongoToSql st1 = new MongoToSql(sourceDB, "sensort1", sqlConn, sender);
+           // st1.run();
         }
         catch (Exception e) {
             System.err.println("BRUH!");
--- java/src/SqlSender.java
+++ java/src/SqlSender.java
@@ -1,16 +1,39 @@
 import org.bson.Document;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
 
 public class SqlSender {
     private Connection connection;
+
+    //apenas estou a considerar o registo de measures, mais tarde faz-se o refactor
+    int id          = 3;
+    String data     = "50.325";
+    int sensor_id   = 1;
+    int zone_id     = 1;
 
     public SqlSender (Connection connection) {
         this.connection = connection;
     }
 
     public synchronized void send (Connection connection, List<Document> list) {
+        // buscar dados e extrair valores
         //enviar para SQL
+        try {
+            String sql = "INSERT INTO measures (id, data, sensor_id, zone_id) VALUES (?, ?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, id);
+            statement.setString(2, data);
+            statement.setInt(3, sensor_id);
+            statement.setInt(4, zone_id);
+            int rows = statement.executeUpdate();
+            System.out.println("Inserted value successfully!!!");
+            statement.close();
+        } catch (Exception e) {
+            System.out.println("Connection failed!!!");
+            e.printStackTrace();
+        }
+
     }
 }
