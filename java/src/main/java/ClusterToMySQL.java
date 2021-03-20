917e52a2027f63454db887fc7658c098da50b69c
rrnr95 <rrnr95@users.noreply.github.com>
2021-03-20 12:43:20 +0000

    updated ClusterToMySQL

--- java/src/main/java/ClusterToMySQL.java
+++ java/src/main/java/ClusterToMySQL.java
@@ -13,33 +13,40 @@ import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 public class ClusterToMySQL {
     private static final String SOURCE_URI = "mongodb+srv://sid2021:sid2021@sid.yingw.mongodb.net/g07?retryWrites=true&w=majority";
+    private static final String SOURCE_DB = "g07";
     private static final String TARGET_URL_CLOUD = "jdbc:mysql://194.210.86.10:3306/aluno_g07_cloud";
     private static final String TARGET_URL_LOCAL = "jdbc:mysql://194.210.86.10:3306/aluno_g07_local";
+    private static final int CADENCE_SECONDS = 5;
 
 
     public static void main(String[] args) {
         try {
 
+            String[] collectionNames = {
+                    "sensort1",
+                    "sensort2",
+            };
+
             // create codec registry for POJOs
             CodecRegistry pojoCodecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
                     fromProviders(PojoCodecProvider.builder().automatic(true).build()));
 
-            // get handle to database
 
-            MongoClient cluster = MongoClients.create(SOURCE_URI);
-            MongoDatabase database = cluster.getDatabase("g07").withCodecRegistry(pojoCodecRegistry);
-
-            //MongoDatabase cluster = MongoClients.create(SOURCE_URI).getDatabase("g07");
             Connection mysqlCloud = DriverManager.getConnection(TARGET_URL_CLOUD, "aluno", "aluno");
             Connection mysqlLocal = DriverManager.getConnection(TARGET_URL_LOCAL, "aluno", "aluno");
 
-            //MongoToSql st1 = new MongoToSql(sourceDB, "sensort1", sqlConn, sender);
-            MongoToSqlPOJO st1 = new MongoToSqlPOJO(database, "sensort1", mysqlCloud, mysqlLocal);
-            st1.start();
+
+            ConnectToMongo from_cluster = new ConnectToMongo(SOURCE_URI, SOURCE_DB);
+            MongoToSQL to_sql = new MongoToSQL(mysqlLocal, new SqlSender(mysqlCloud), CADENCE_SECONDS);
+
+            from_cluster.useCollections(collectionNames);
+
+            from_cluster.startFetching();
+
+            to_sql.serveSQL(from_cluster.getFetchingSource());
+
         } catch (Exception e) {
             System.err.println("BRUH!");
         }
-
-
     }
 }
