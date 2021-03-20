761cbeb27b234faebd829260cf0f1705814d4f5e
rrnr95 <rrnr95@users.noreply.github.com>
2021-03-20 12:46:15 +0000

    updated MongoTreatment
    esta classe é para APAGAR

--- java/src/main/java/MongoTreatment.java
+++ java/src/main/java/MongoTreatment.java
@@ -4,41 +4,10 @@ import org.bson.types.ObjectId;
 
 import java.util.concurrent.LinkedBlockingQueue;
 
-public class MongoTreatment extends ConnectToMongo.DocumentPublisher {
-
-    private final LinkedBlockingQueue<Document> buffer;
-
-    MongoTreatment(MongoCollection<Document> collection, LinkedBlockingQueue<Document> buffer) {
-        super(collection, buffer);
-        this.buffer = buffer;
-    }
-
-    @Override
-    public void run() {
-
-        try {
-            Thread.sleep(5000);
-            float media, acc = 0;
-            int counter = 0;
-            while (buffer.peek() != null){
-
-                Document doc = buffer.poll();
-
-                //accumula o valor da medição lida
-                acc += doc.get("Medicao", Float.class);
-
-                //conta a acumulação para depois dividir no fim
-                counter++;
-            }
-            media = acc/counter;
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+public class MongoTreatment{
 
     //Extrai os campos do doc e instancía uma measurement com os mesmos
-    private static MeasurementPOJO convertDocToMeasurement(Document document){
+    public static MeasurementPOJO convertDocToMeasurement(Document document){
         ObjectId id = document.getObjectId("_id");
         String zone = document.getString("Zona");
         String sensor = document.getString("Sensor");
