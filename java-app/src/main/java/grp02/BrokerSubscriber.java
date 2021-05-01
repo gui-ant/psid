d5e879d2529df6f0dbf5edadca071aa2c357f926
rrnr95 <rrnr95@users.noreply.github.com>
2021-05-01 20:51:32 +0100

    print to println fix

--- java-app/src/main/java/grp02/BrokerSubscriber.java
+++ java-app/src/main/java/grp02/BrokerSubscriber.java
@@ -33,7 +33,7 @@ public class BrokerSubscriber extends BrokerConnector {
             public void messageArrived(String topic, MqttMessage message) throws Exception {
 
                 String[] message_info = MyUtils.messageIntoArray(message);
-                for (String s: message_info) { System.out.print(s); }
+                for (String s: message_info) { System.out.println(s); }
 
                 try {
                     Measurement measurement = MyUtils.buildMeasurement(message_info);
