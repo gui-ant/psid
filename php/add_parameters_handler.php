1ef6ef1d0405af99db82a3f61b08146f7b4aee00
guiantz <gantunes90@gmail.com>
2021-05-05 03:35:20 +0100

    simplificacoes e correcoes

--- php/add_parameters_handler.php
+++ php/add_parameters_handler.php
@@ -3,32 +3,24 @@ include('db/config.php');
 //echo var_dump($_POST['culture_id']);
 
 // verifica se existe pelo menos uma checkbox assinalada
-if (isset($_POST['hum']) || isset($_POST['temp']) || isset($_POST['luz'])){
+if (isset($_POST['hum']) || isset($_POST['temp']) || isset($_POST['luz'])) {
 
     $params = array();
 
     if (isset($_POST['hum'])) {
 
-        //create_param("H", $_POST['min_h'], $_POST['max_h'], $_POST['tol_h'], $set_id, $param_id);
-        $param_hum = '"H",' . $_POST['max_h'] . ',' . $_POST['min_h'] . ',' . $_POST['tol_h'] . ',@set_id,@param_id);';
-        array_push($params, $param_hum);
-        
-
+        //create_param("H", $_POST['min_h'], $_POST['max_h'], $_POST['tol_h']);
+        array_push($params, "'H', $_POST[max_h], $_POST[min_h], $_POST[tol_h]");
     }
 
     if (isset($_POST['temp'])) {
 
-        //$param_id_temp = (int)create_param("T", $_POST['min_t'], $_POST['max_t'], $_POST['tol_t'], $set_id, $param_id);
-        $param_temp = '"T",' . $_POST['max_t'] . ',' . $_POST['min_t'] . ',' . $_POST['tol_t'] . ',@set_id,@param_id);';
-        array_push($params, $param_temp);
-
+        //$param_id_temp = (int)create_param("T", $_POST['min_t'], $_POST['max_t'], $_POST['tol_t']);
+        array_push($params, "'T', $_POST[max_t], $_POST[min_t], $_POST[tol_t]");
     }
 
     if (isset($_POST['luz'])) {
-
-        $param_luz = '"L",' . $_POST['max_l'] . ',' . $_POST['min_l'] . ',' . $_POST['tol_l'] . ',@set_id,@param_id);';
-        array_push($params, $param_luz);
-
+        array_push($params, "'L', $_POST[max_l], $_POST[min_l], $_POST[tol_l]");
     }
 
     create_params($params);
@@ -42,37 +34,31 @@ if (isset($_POST['hum']) || isset($_POST['temp']) || isset($_POST['luz'])){
     if (isset($_POST['submit'])) {
         echo '<div class="u-form-send-error u-form-send-message"> Please select at least one of the checkboxes (Humidity, Temperature, Light) </div>';
     }
-
 }
 
-function create_params($params) {
+function create_params($params)
+{
 
     $conn = db_connect();
 
     if (!$conn) {
-        die("Connection failed: " .$conn->connect_error);
+        die("Connection failed: " . $conn->connect_error);
         return false;
-      }
-
-    $query = "
-    SET @user_id=" . $_SESSION['user_id'] . ";SET @set_id=0;SET @param_id=0;
-    call spCreateCultureParamsSet(" . $_POST['culture_id'] . ",@set_id);";
-
-    foreach($params as $p) {
-        $query = $query . "call spCreateCultureParam(" . $p;
     }
-      //echo var_dump($query) . "<br>";
-    
+
+    $query = "SET @set_id=0; SET @param_id=0; CALL spCreateCultureParamsSet(" . $_POST['culture_id'] . ", @set_id);";
+
+    foreach ($params as $p) {
+        $query = $query . "CALL spCreateCultureParam(" . $p . ", @set_id, @param_id);";
+    }
+    //echo var_dump($query) . "<br>";
+
     $results = mysqli_multi_query($conn, $query);
     mysqli_close($conn);
 
-    if ($results){
+    if ($results) {
         echo '<div class="u-form-send-message u-form-send-success"> Your form has been successfully submitted :) </div>';
     } else {
         echo '<div class="u-form-send-error u-form-send-message"> Unable to send your data. Please fix errors then try again. </div>';
     }
-
-} 
-
-
-?>
+}
--- php/cultures.php
+++ php/cultures.php
@@ -54,15 +54,24 @@ function edit_culture_data($culture_id, $culture_name)
 <?php
 $url = "localhost/psid/php/db/getStoredProcData.php?sp=spGetCulturesByUserId&p=" . $_SESSION['user_id'] . "&json=true";
 $res = db_curl_request($url);
-$pieces = json_decode($res);
+$user_cultures = json_decode($res);
+
+if ($culture_id != "") {
+	$url = "localhost/psid/php/db/getStoredProcData.php?sp=spGetCultureById&p=" . $culture_id . "&json=true";
+	$res = db_curl_request($url);
+	$active_culture = json_decode($res);
+	
+	// echo var_dump($response);
+	// echo var_dump($pieces);
+}
 ?>
 
-<?php if (!is_null($pieces)) : ?>
+<?php if (!is_null($user_cultures)) : ?>
 	<form name="Combobox" action="index.php" method="post">
 		<div class="input-group">
 			<select name="culture_id" id="itens" onchange="this.form.submit()">
 				<option>Escolha uma cultura:</option>
-				<?php foreach ($pieces as $i => $piece) : ?>
+				<?php foreach ($user_cultures as $i => $piece) : ?>
 					<?php if (isset($_POST['culture_id'])) : ?>
 						<option value="<?= $piece->id; ?>" <?= $_POST['culture_id'] == $piece->id ? 'selected' : ''; ?>><?= $piece->name; ?></option>
 					<?php else : ?>
@@ -78,41 +87,32 @@ $pieces = json_decode($res);
 	<p>Não tem culturas atribuidas.</p>
 <?php endif; ?>
 
-<?php
-if ($culture_id != "") {
-	$url = "localhost/psid/php/db/getStoredProcData.php?sp=spGetCultureById&p=" . $culture_id . "&json=true";
-	$res = db_curl_request($url);
-	$pieces = json_decode($res);
-	
-	// echo var_dump($response);
-	// echo var_dump($pieces);
-}
-?>
 
-<?php if (!is_null($pieces)) : ?>
+
+<?php if (isset($active_culture)) : ?>
 	Dados da Cultura
 	<form action="index.php" method="POST" class="form-group">
 		<br>
 		<div class="input-group">
 			<label>ID cultura:</label>
-			<input type="text" name="culture_id" value="<?= $pieces[0]->id; ?>" readonly>
+			<input type="text" name="culture_id" value="<?= $active_culture[0]->id; ?>" readonly>
 		</div>
 		<div class="input-group">
 			<label>Nome cultura:</label>
-			<input type="text" name="culture_name" value="<?= $pieces[0]->name; ?>" <?= $pieces[0]->manager_id == $_SESSION['user_id'] ? : "readonly" ?>>
+			<input type="text" name="culture_name" value="<?= $active_culture[0]->name; ?>" <?= $active_culture[0]->manager_id == $_SESSION['user_id'] ? : "readonly" ?>>
 		</div>
 		
-		<?php if ($pieces[0]->manager_id == $_SESSION['user_id']) : ?>
+		<?php if ($active_culture[0]->manager_id == $_SESSION['user_id']) : ?>
 			<input class="btn input-group" type="submit" name="submit" value="Gravar">
 			<input type="hidden" name="update_culture">
 			<input type="hidden" name="goback" value="<?php echo $_POST['goback']; ?>">
 		<?php endif ?>
 	</form>
 
-	<?php if ($pieces[0]->manager_id == $_SESSION['user_id']) : ?>
+	<?php if ($active_culture[0]->manager_id == $_SESSION['user_id']) : ?>
 		<form action="add_parameters.php" method="POST">
-			<input type="hidden" name="culture_id" value="<?= $pieces[0]->id; ?>">
-			<input type="hidden" name="culture_name" value="<?= $pieces[0]->name; ?>">
+			<input type="hidden" name="culture_id" value="<?= $active_culture[0]->id; ?>">
+			<input type="hidden" name="culture_name" value="<?= $active_culture[0]->name; ?>">
 			<input class="btn" type="submit" value="Adicionar Parâmetros"/>
 		</form>
 	<?php endif ?>
