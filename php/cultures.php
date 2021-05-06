888b1845bd110e803ef5d5fc99962b551ba5ddf8
erikaevora <erika.evora@gmail.com>
2021-05-06 13:02:32 +0100

    Update styling index and cultures

--- php/cultures.php
+++ php/cultures.php
@@ -1,5 +1,6 @@
 <?php
 
+
 $culture_id = "";
 $pieces = null;
 
@@ -109,74 +110,133 @@ if ($culture_id != "") {
 }
 ?>
 
-<?php if (!is_null($user_cultures)) : ?>
-	<form name="Combobox" action="index.php" method="post">
-		<div class="input-group">
-			<select name="culture_id" id="itens" onchange="this.form.submit()">
-				<option>Escolha uma cultura:</option>
-				<?php foreach ($user_cultures as $i => $piece) : ?>
-					<?php if (isset($_POST['culture_id'])) : ?>
-						<option value="<?= $piece->id; ?>" <?= $_POST['culture_id'] == $piece->id ? 'selected' : ''; ?>><?= $piece->name; ?></option>
-					<?php else : ?>
-						<option value="<?= $piece->id; ?>"><?= $piece->name; ?></option>
-					<?php endif; ?>
-				<?php endforeach; ?>
-			</select>
-		</div>
-		<!-- <input class="btn" type="submit" name="btnEnvia" value="Ver">-->
-		<input class="btn" type="hidden" name="goback" value="<?php echo $_SERVER['REQUEST_URI'] ?>">
-	</form>
-<?php else : ?>
-	<p>Não tem culturas atribuidas.</p>
-<?php endif; ?>
 
-<?php if (isset($active_culture)) : ?>
-	Dados da Cultura
-	<form action="index.php" method="POST" class="form-group">
-		<br>
-		<div class="input-group">
-			<label>ID cultura:</label>
-			<input type="text" name="culture_id" value="<?= $active_culture[0]->id; ?>" readonly>
-		</div>
-		<div class="input-group">
-			<label>Nome cultura:</label>
-			<input type="text" name="culture_name" value="<?= $active_culture[0]->name; ?>" <?= $active_culture[0]->manager_id == $_SESSION['user_id'] ?: "readonly" ?>>
-		</div>
+ 
+    <section class="u-align-center u-clearfix u-grey-90 u-section-1" id="carousel_b2e3">
+      <div class="u-clearfix u-sheet u-sheet-1">
+      
 
-		<?php if ($active_culture[0]->manager_id == $_SESSION['user_id']) : ?>
-			<input class="btn input-group" type="submit" name="submit" value="Gravar">
-			<input type="hidden" name="update_culture">
-			<input type="hidden" name="goback" value="<?php echo $_POST['goback']; ?>">
-		<?php endif ?>
-	</form>
+        <div class="u-form u-form-1">
 
+        <?php if (!is_null($user_cultures)) : ?>
+          
+        <form action="index.php" method="POST" class="u-clearfix u-form-spacing-15 u-form-vertical u-inner-form" style="padding: 0px;" source="custom" name="Combobox">
+            <div class="u-form-group u-form-select u-form-group-1">
+              <div class="u-form-select-wrapper">
 
-<?php endif ?>
+              <select name="culture_id" id="itens" onchange="this.form.submit()" class="u-border-2 u-border-grey-90 u-custom-font u-input u-input-rectangle u-palette-1-dark-1 u-text-font u-input-1" >
+			
+							<option>Pick a culture:</option>
+							<?php foreach ($user_cultures as $i => $piece) : ?>
+								<?php if (isset($_POST['culture_id'])) : ?>
+									<option value="<?= $piece->id; ?>" <?= $_POST['culture_id'] == $piece->id ? 'selected' : ''; ?>><?= $piece->name; ?></option>
+								<?php else : ?>
+									<option value="<?= $piece->id; ?>"><?= $piece->name; ?></option>
+								<?php endif; ?>
+							<?php endforeach; ?>
+							
+							</select>
 
-<?php if (isset($params_sets)) : ?>
-	Parâmetros da Cultura
-	<?php if ($active_culture[0]->manager_id == $_SESSION['user_id']) : ?>
-		<form action="add_parameters.php" method="POST">
-			<input type="hidden" name="culture_id" value="<?= $active_culture[0]->id; ?>">
-			<input type="hidden" name="culture_name" value="<?= $active_culture[0]->name; ?>">
-			<input class="btn" type="submit" value="Adicionar Parâmetros" />
-		</form>
-	<?php endif ?>
-	<form action="index.php" method="POST">
-		<?php foreach ($params_sets as $i => $param) : ?>
-			<div class="input-group" style="border: 3px dashed grey;border-radius: 6px;margin-bottom:10px">
-				<?php foreach ($param as $p) : ?>
-					<label for="chk_param_<?= $param[0]->id; ?>">
-						<span></span><input id="chk_param_<?= $param[0]->id; ?>" type="checkbox" name="chk_param[]" value="<?= $param[0]->id; ?>" <?= $active_culture[0]->manager_id == $_SESSION['user_id'] ?: "disabled" ?>>
-						<?= "Sensor Type: $p->sensor_type, Min. Val.: $p->valmin, Max. Val.: $p->valmax, Tolerance: $p->tolerance; " ?>
-					</label>
-				<?php endforeach; ?>
-				</label>
-			</div>
-		<?php endforeach; ?>
-		<?php if ($active_culture[0]->manager_id == $_SESSION['user_id'] && count($params_sets) > 0) : ?>
-			<input class="btn input-group" type="submit" name="submit" value="Eliminar">
-			<input type="hidden" name="delete_param">
-		<?php endif ?>
-	</form>
-<?php endif ?>
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="12" version="1" class="u-caret"><path fill="currentColor" d="M4 8L0 4h8z"></path></svg>
+              </div>
+            
+            <input type="hidden" name="goback" value="<?php echo $_SERVER['REQUEST_URI'] ?>">
+          </form>
+
+        </div>
+      <?php else : ?>
+				<p>You don't have any cultures yet.</p>
+			<?php endif; ?>
+      </div>
+    </section>
+
+    <?php if (isset($active_culture)) : ?>
+    <section class="u-align-center u-clearfix u-grey-90 u-section-2" id="sec-ff2f">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h5 class="u-align-center u-text u-text-1">Culture's data</h5>
+        <div class="u-border-2 u-border-grey-50 u-container-style u-group u-shape-rectangle u-group-1">
+          <div class="u-container-layout u-container-layout-1">
+            <div class="u-form u-form-1">
+              
+            <form action="index.php" method="POST" class="u-clearfix u-form-spacing-12 u-form-vertical u-inner-form" style="padding: 0;" source="custom" name="form">
+                <div class="u-form-group u-form-group-1">
+                  <label for="text-6fa8" class="u-label u-label-1">Culture's id:</label>
+                  <input type="text" id="text-6fa8" name="culture_id" class="u-input u-input-1" value="<?= $active_culture[0]->id; ?>" readonly>
+                </div>
+                <div class="u-form-group u-form-group-2">
+                  <label for="text-6ea0" class="u-label u-label-2">Culture's name:</label>
+                  <input type="text" id="text-6ea0" name="culture_name" class="u-input u-input-1" value="<?= $active_culture[0]->name; ?>" <?= $active_culture[0]->manager_id == $_SESSION['user_id'] ?: "readonly" ?>>
+                </div>
+                <?php if ($active_culture[0]->manager_id == $_SESSION['user_id']) : ?>
+                <div class="u-align-right u-form-group u-form-submit u-form-group-3">
+                  <!--<a href="#" class="u-black u-btn u-btn-rectangle u-btn-submit u-button-style u-hover-palette-1-dark-1 u-btn-1">Save</a>
+                  <input type="submit" value="submit" class="u-form-control-hidden">-->
+                  <input type="submit" value="Save" class="u-black u-btn u-btn-rectangle u-btn-submit u-button-style u-hover-palette-1-dark-1 u-btn-1">
+                </div>
+                <?php endif ?>
+                
+            </form>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <?php endif ?>
+
+    <?php if (isset($params_sets)) : ?>
+    <section class="u-align-center u-clearfix u-grey-90 u-section-3" id="carousel_7d02">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h5 class="u-align-center u-text u-text-1">Culture's Parameters</h5>
+        <div class="u-border-2 u-border-grey-50 u-container-style u-group u-shape-rectangle u-group-1">
+          <div class="u-container-layout u-container-layout-1">
+            
+            <div class="u-form u-form-1">
+              <form action="index.php" method="POST" class="u-clearfix u-form-spacing-12 u-form-vertical u-inner-form" style="padding: 0;" source="custom" name="form">
+              
+              <?php foreach ($params_sets as $i => $param) : ?>
+                <div class="input-group" style="border: 3px dashed grey;border-radius: 6px;margin-bottom:10px; padding:3px;">
+                  <div class="u-form-checkbox u-form-group u-form-group-1">
+                    <?php foreach ($param as $p) : ?>
+                      <label for="chk_param_<?= $param[0]->id; ?>" class="u-label u-label-1">
+                        <span></span><input id="chk_param_<?= $param[0]->id; ?>" type="checkbox" name="chk_param[]" value="<?= $param[0]->id; ?>" <?= $active_culture[0]->manager_id == $_SESSION['user_id'] ?: "disabled" ?> class="u-block-45b7-76 u-border-2 u-border-grey-75 u-border-no-left u-border-no-right u-border-no-top u-input-rectangle">
+                        <?= "Sensor Type: $p->sensor_type, Min. Val.: $p->valmin, Max. Val.: $p->valmax, Tolerance: $p->tolerance; " ?>
+                      </label>
+                    <?php endforeach; ?>
+                  </div>
+                </div>
+              <?php endforeach; ?>
+
+              <!--<div class="u-form-checkbox u-form-group u-form-group-2">
+                  <input type="checkbox" id="checkbox-703f" name="checkbox" value="On">
+                  <label for="checkbox-703f" class="u-label u-label-2">[Parameter 2]</label>
+                </div>-->
+                
+              <?php if ($active_culture[0]->manager_id == $_SESSION['user_id'] && count($params_sets) > 0) : ?>
+                <div class="u-align-right u-form-group u-form-submit u-form-group-6">
+                  <!--<a href="#" class="u-black u-btn u-btn-rectangle u-btn-submit u-button-style u-hover-palette-1-dark-1 u-btn-1">Delete</a>
+                  <input type="submit" value="submit" class="u-form-control-hidden">-->
+                  <input type="submit" value="Delete" class="u-black u-btn u-btn-rectangle u-btn-submit u-button-style u-hover-palette-1-dark-1 u-btn-1">
+                  <input type="hidden" name="delete_param">
+                </div>
+              <?php endif ?>
+                
+              </form>
+            </div>
+          </div>
+        </div>
+
+        <?php if ($active_culture[0]->manager_id == $_SESSION['user_id']) : ?>
+					<form action="add_parameters.php" method="POST">
+						<input type="hidden" name="culture_id" value="<?= $active_culture[0]->id; ?>">
+						<input type="hidden" name="culture_name" value="<?= $active_culture[0]->name; ?>">
+            <!--<a href="#" class="u-black u-btn u-button-style u-hover-palette-1-dark-1 u-btn-2">Add parameters</a>
+            <input type="submit" value="submit" class="u-form-control-hidden">-->
+						<input type="submit" class="u-black u-btn u-button-style u-hover-palette-1-dark-1 u-btn-2" value="Add Parameters" />
+					</form>
+				<?php endif ?>
+        
+      </div>
+    </section>
+    <?php endif ?>
+    
--- php/index.php
+++ php/index.php
@@ -12,23 +12,41 @@ if (isset($_GET['logout'])) {
     header("location: auth/login.php");
 }
 ?>
+
+
 <!DOCTYPE html>
-<html>
-
-<head>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Udpate Culture">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
     <title>Home</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-
-<body>
-
-    <div class="header">
-        <h2>Home</h2>
-    </div>
-    <div class="content">
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+    <link rel="stylesheet" href="index-cultures.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 3.9.3, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "Home",
+		"url": "index.html"
+    }</script>
+    <meta property="og:title" content="update culture">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#478ac9">
+    <link rel="canonical" href="index.html">
+    <meta property="og:url" content="index.html">
+  </head>
+  <body class="u-body u-palette-1-base">
+  
         <!-- notification message -->
         <?php if (isset($_SESSION['success'])) : ?>
-            <div class="error success">
+            <div style="color:#3c763d;background:#dff0d8;border:1px,solid,#3c763d;margin-bottom:20px;">
                 <h3>
                     <?php
                     echo $_SESSION['success'];
@@ -39,29 +57,34 @@ if (isset($_GET['logout'])) {
         <?php endif ?>
 
         <?php if (isset($_SESSION['user_email'])) : ?>
-            <p>
-                Bem-vindo <strong><?php echo $_SESSION['user_name']; ?></strong>&nbsp;<a href="?logout='1'">Sair</a>
-            </p>
+            <section class="u-align-center u-clearfix u-grey-90 u-section-1" id="carousel_b2e3">
 
+                <div class="u-clearfix u-sheet u-sheet-1">
 
-            <?php if ($_SESSION['user_role'] == 'group_researcher') : ?>
-                <!-- Researcher View -->
-                <p>
-                    <?php include("cultures.php");?>
-                </p>
+                    <h2 class="u-text u-text-body-alt-color u-text-1">Welcome <?php echo $_SESSION['user_name']; ?></h2>
+                    <a href="?logout='1'" class="u-black u-btn u-button-style u-hover-palette-1-dark-1 u-btn-1">Logout<br></a>
+                    
+                    
+                    
+                    <?php if ($_SESSION['user_role'] == 'group_researcher') : ?>
+                        <!-- Researcher View -->
+                        <p>
+                            <?php include("cultures.php");?>
+                        </p>
 
-            <?php elseif ($_SESSION['user_role'] == 'group_admin') : ?>
-                <!-- Admin View -->
-                <p>Painel de Admnistração</p>
-            <?php elseif ($_SESSION['user_role'] == NULL) : ?>
-                <!-- User with no roles View -->
-                <p>
-                    Ainda não tem perfil atribuído. Contacte um administrador.
-                </p>
-            <?php endif ?>
-        <?php endif ?>
-    </div>
+                    <?php elseif ($_SESSION['user_role'] == 'group_admin') : ?>
+                        <!-- Admin View -->
+                        <p class="u-align-center u-text u-text-2">Administration Panel</p>
+                    <?php elseif ($_SESSION['user_role'] == NULL) : ?>
+                        <!-- User with no roles View -->
+                        <p>
+                            Ainda não tem perfil atribuído. Contacte um administrador.
+                        </p>
+                    <?php endif ?>
+                    </div>
 
-</body>
-
+            </section>
+         <?php endif ?>
+        
+  </body>
 </html>
