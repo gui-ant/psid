e693b39f7781c2e20b7e61d0b74cad65b9bb6577
guiantz <gantunes90@gmail.com>
2021-05-08 19:56:04 +0100

    reajuste layout login

--- php/auth/login.php
+++ php/auth/login.php
@@ -7,26 +7,27 @@
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
     <link href="../css/styles.css" rel="stylesheet" type="text/css" media="screen">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
 </head>
 
 <body class="body-signin">
     <section class="form-signin">
         <div class="container">
             <form method="post" action="login.php">
-                <h1 class="h1 mb-3 fw-bolder">LOGIN</h1>
+                <h2 class="display-1">LOGIN</h2>
                 <?php if (count($errors) > 0) : ?>
-                    <div class="alert alert-error">
+                    <div class="alert alert-danger">
                         <?php foreach ($errors as $error) : ?>
                             <p><?php echo $error ?></p>
                         <?php endforeach ?>
                     </div>
                 <?php endif ?>
                 <div class="form-group">
-                    <label for="emaiInput">Email</label>
+                    <label for="emaiInput" class="form-label">Email</label>
                     <input type="email" name="email" value="<?= isset($_SESSION['email']) ? $_SESSION['email'] : "" ?>" class="form-control" id="emaiInput" placeholder="Enter your e-mail">
                 </div>
                 <div class="form-group">
-                    <label for="passwordInput">Password</label>
+                    <label for="passwordInput" class="form-label">Password</label>
                     <input type="password" name="pass" value="" class="form-control" id="passwordInput" placeholder="Enter your password">
                 </div>
                 <div class="d-grid col-4 mx-auto">
