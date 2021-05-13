5e3d6ecf20b82865e7b151b85ac23f25dbda0538
DiogoFrnc <diogojfrancisco28@gmail.com>
2021-05-14 00:52:11 +0100

    android scripts

--- android/scripts/getMedicoesTemperatura.php
+++ android/scripts/getMedicoesTemperatura.php
@@ -1,9 +1,28 @@
 	<?php
 	$url="127.0.0.1";
-	$database="pisid2021"; // Alterar nome da BD se necessario
-    $conn = mysqli_connect($url,$_POST['username'],$_POST['password'],$database);
+	$database="g07_local"; // Alterar nome da BD se necessario
+	
+	$url2="194.210.86.10";
+	$database2="aluno_g07_cloud";
+	
+   // $conn = mysqli_connect($url,$_POST['username'],$_POST['password'],$database);
+	$conn = mysqli_connect($url,'root','',$database);
+	
+	$conn_cloud = mysqli_connect($url2,'aluno','aluno',$database2);
+	$get_temp_ids_sql="SELECT id FROM sensors where substring(name,1,1) = 'T'";
+	$ids_res = mysqli_query($conn_cloud, $get_temp_ids_sql);
+	
+	$array_id = array();
+	while($sns_id=mysqli_fetch_assoc($ids_res)){
+		// Adicionar ao array o id de sensor de temperatura
+		array_push($array_id, $sns_id['id']);
+	}
+	
+
+	
 	// Completar o select para ir buscar a hora e leituras de temperatura da tabela Medicao dos ultimos 5 minutos
-	$sql = "SELECT ... where ... AND Hora >= now() - interval 5 minute ORDER BY Hora ASC";
+	$sql = "SELECT date, value FROM measurements where sensor_id IN (".Join(",",$array_id).") AND date >= now() - interval 5 minute ORDER BY date ASC";
+	echo $sql;
 	$result = mysqli_query($conn, $sql);
 	$response["medicoes"] = array();
 	if ($result){
@@ -11,8 +30,8 @@
 			while($r=mysqli_fetch_assoc($result)){
 				$ad = array();
 				// Adicionar ao array a hora e leitura
-				$ad["Hora"] = $r['Hora'];
-				$ad["Leitura"] = $r['Leitura'];
+				$ad["date"] = $r['date'];
+				$ad["value"] = $r['value'];
 				array_push($response["medicoes"], $ad);
 			}
 		}	
--- android/scripts/validateLogin.php
+++ android/scripts/validateLogin.php
@@ -1,13 +1,14 @@
 	<?php
 	$url="127.0.0.1";
 	$database="g07_local"; // Alterar nome da BD se necessario
-    $conn = mysqli_connect($url,$_POST['username'],$_POST['password'],$database);
+    $conn = mysqli_connect($url,"res1@foo.bar","pass",$database);
 	$query_select = "SELECT current_role() as role";
 	$result_query_select = mysqli_query($conn, $query_select); 
 	$conn->next_result();
 	$role = mysqli_fetch_assoc($result_query_select)['role'];
 	if($role == "group_researcher") { // Alterar role se necessario
 		$response["valid"] = array();
+		array_push($response["valid"],"boa!");
 		$json = json_encode($response["valid"]);
 		echo $json;
 	}
