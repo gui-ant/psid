e3ce8f00a97bb8dcec94fa7acb26dede292d17c3
DiogoFrnc <diogojfrancisco28@gmail.com>
2021-05-14 01:53:01 +0100

    scripts update

--- android/scripts/getMedicoesTemperatura.php
+++ android/scripts/getMedicoesTemperatura.php
@@ -6,7 +6,7 @@
 	$database2="aluno_g07_cloud";
 	
    // $conn = mysqli_connect($url,$_POST['username'],$_POST['password'],$database);
-	$conn = mysqli_connect($url,'root','',$database);
+    $conn = mysqli_connect($url,$_POST['username'],$_POST['password'],$database);
 	
 	$conn_cloud = mysqli_connect($url2,'aluno','aluno',$database2);
 	$get_temp_ids_sql="SELECT id FROM sensors where substring(name,1,1) = 'T'";
@@ -22,7 +22,6 @@
 	
 	// Completar o select para ir buscar a hora e leituras de temperatura da tabela Medicao dos ultimos 5 minutos
 	$sql = "SELECT date, value FROM measurements where sensor_id IN (".Join(",",$array_id).") AND date >= now() - interval 5 minute ORDER BY date ASC";
-	echo $sql;
 	$result = mysqli_query($conn, $sql);
 	$response["medicoes"] = array();
 	if ($result){
--- android/scripts/validateLogin.php
+++ android/scripts/validateLogin.php
@@ -1,7 +1,7 @@
 	<?php
 	$url="127.0.0.1";
 	$database="g07_local"; // Alterar nome da BD se necessario
-    $conn = mysqli_connect($url,"res1@foo.bar","pass",$database);
+    $conn = mysqli_connect($url,$_POST['username'],$_POST['password'],$database);
 	$query_select = "SELECT current_role() as role";
 	$result_query_select = mysqli_query($conn, $query_select); 
 	$conn->next_result();
